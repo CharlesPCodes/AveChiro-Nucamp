cad023b3a952f5c81fe631868fbb0e2620c3f795
Charles <charles.pierson15@gmail.com>
2020-02-22 22:29:28 -0500

    Updates

--- js/greeting.js
+++ js/greeting.js
@@ -2,11 +2,11 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
-if(hourNow = 18){
+if(hourNow > 18){
     greeting = 'Good Evening!';
-}else if(hourNow = 12){
+}else if(hourNow > 12){
     greeting = 'Good Afternoon!';
-}else if(hourNow = 0){
+}else if(hourNow >= 0){
     greeting = 'Good Morning!'
 }else{
     greeting= 'Welcome!'
