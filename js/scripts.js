99881a21f02c73f6b1e9989865bfc380acbb9ffa
Charles Pierson <charles.pierson15@gmail.com>
2020-02-26 01:33:36 -0500

    Site Design changes + New Pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
-$('#card1Button').click(function(){
-    $('#card1Modal').modal('show');
-});
-
-$('#massageButton').click(function(){
-    $('#massageModal').modal('show');
+$(function(){
+    $(".carousel").carousel({
+        interval: 2500
+    });
+    $('#communityButton').click(function () {
+        $('#communityModal').modal('show');
+    });
+    
+    $('#massageButton').click(function () {
+        $('#massageModal').modal('show');
+    });
+    
+    $('#contactButton').click(function () {
+        $('#contactModal').modal('show');
+    });
+    
 });
